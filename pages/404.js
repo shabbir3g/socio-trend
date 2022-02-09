fb8b2c5aea317ab2dc5cbb012a4ae1e1965d3918
saddaul-siam <saddadul.siam@gmail.com>
2022-02-09 19:05:34 +0600

    added 404

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,25 @@
-import Image from "next/Image"
-import styles from "../styles/Error.module.css"
+import Image from "next/Image";
+import styles from "../styles/Error.module.css";
 
 const Error = () => {
-    return (
-        <div style={{textAlign:"center", marginTop:'20vh'}}>
-            <div >
-                <Image src="/errorImg.png" width="200px" height="250px"></Image>
-            </div>
-            <div className={styles.errorText}>
-                <p className={styles.errorTitle}>Oops! It looks like <br />.you&apos;re lost</p>
-                <p className={styles.errorSms}>.The page you&apos;re looking for isn&apos;t available. Try to search again or use the go to</p>
-                <button>HOME PAGE</button>
-            </div>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh" }}>
+      <div>
+        <Image src="/errorImg.png" width="200px" height="250px" alt="" />
+      </div>
+      <div className={styles.errorText}>
+        <p className={styles.errorTitle}>
+          Oops! It looks like <br />
+          .you&apos;re lost
+        </p>
+        <p className={styles.errorSms}>
+          .The page you&apos;re looking for isn&apos;t available. Try to search
+          again or use the go to
+        </p>
+        <button>HOME PAGE</button>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
