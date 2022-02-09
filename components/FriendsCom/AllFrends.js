604839560208abeee0acedf50a696192ca9f5972
omarali18 <omarali1814000@gmail.com>
2022-02-10 00:06:48 +0600

    search field emplement

--- components/FriendsCom/AllFrends.js
+++ components/FriendsCom/AllFrends.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleFrends from './SingleFrends';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilter, faSearchDollar, faSearchPlus } from '@fortawesome/free-solid-svg-icons'
@@ -53,35 +53,35 @@ const friends = [
         role:"ADD FRIEND"
     },
     {
-        id: 4,
+        id: 7,
         name: "Opi Barua",
         userId:"@opi234",
         img:"https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
         role:"FRIEND"
     },
     {
-        id: 5,
+        id: 8,
         name: "Parvez Islam Shuvo",
         userId:"@parvez234",
         img:"https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
         role:" ADD FRIEND"
     },
     {
-        id: 1,
+        id: 9,
         name: "Omar Ali",
         userId:"@omarali234",
         img:"https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
         role:"ADD FRIEND"
     },
     {
-        id: 2,
+        id: 10,
         name: "Bepari Prantosh",
         userId:"@prantosh234",
         img:"https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
         role:"FRIEND"
     },
     {
-        id: 3,
+        id: 11,
         name: "Md. Mostafizur Rahman",
         userId:"@mostafizur234",
         img:"https://thumbs.dreamstime.com/b/handsome-man-hair-style-beard-beauty-face-portrait-fashion-male-model-black-hair-high-resolution-handsome-man-125031765.jpg",
@@ -90,9 +90,23 @@ const friends = [
 ]
 
 const AllFrends = () => {
-    const styleSearch = {
+    const [members, setMembers] = useState([])
+    const [searchMember, setSearchMember] = useState([])
+    // setMember(friends)
+    // setSearchMember(friends)
+    useEffect( ()=>{
+        setMembers(friends)
+    setSearchMember(friends)
+    } ,[])
+    console.log("members", members);
 
+    const searchFiealdhandle = (e) => {
+        const searchText = e.target.value
+
+        const metchedProduct = members.filter(member => member.name.toLowerCase().includes(searchText.toLowerCase()))
+        setSearchMember(metchedProduct)
     }
+    
     return (
         <div className='bg-slate-200 pb-20'>
             <div className="mx-8 md:mx-18 sm:mx-11 xs:mx-8 lg:mx-28 pt-5">
@@ -102,11 +116,10 @@ const AllFrends = () => {
                     <div className='flex justify-center justify-items-center'>
                         <div className=' bg-gray-100 w-72 h-12 rounded-lg relative'>
                         <form >
-                            <input type="text" placeholder='Search here.' className='w-full h-12 block rounded-lg pl-4 text-lg border-0 outline-0 bg-gray-100' />
+                            <input onChange={searchFiealdhandle} type="text" placeholder='Search here.' className='w-full h-12 block rounded-lg pl-4 text-lg border-0 outline-0 bg-gray-100' />
                             <button className='absolute right-0 top-0 w-16 h-full rounded-full text-xl '>{filter} </button>
                         </form>
                         </div>
-                        {/* <input type="text" className='border bg-red-200' /> */}
                             {/* <i className="fa-solid fa-magnifying-glass"></i> */}
                         <div className='text-center bg-gray-100 w-12 ml-4 rounded-lg'>
                         <h1 className='text-xl pt-2.5'>{filter}</h1>
@@ -115,7 +128,7 @@ const AllFrends = () => {
                 </div>
                 <div className="grid grid-cols-1 xs:grid-cols-12 sm:grid-cols-12 md:grid-cols-12 gap-3">
                 {
-                        friends.map(friend => <SingleFrends
+                        searchMember.map(friend => <SingleFrends
                             friend={friend}
                             key={friend.id}
 
