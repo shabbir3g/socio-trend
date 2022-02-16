72a8891fab6600c85e7b6b10626a889ee43d935e
saddaul-siam <saddadul.siam@gmail.com>
2022-02-16 12:02:59 +0600

    profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,19 +1,34 @@
+import Head from "next/head";
 import React from "react";
+import LeftSideBar from "../components/Home/LeftSideBar";
+import RightSideBar from "../components/Home/RightSideBar";
+import Navigation from "../components/Share/Navigation";
 import UserProfile from "../components/userProfile/UserProfile";
 
 const userProfile = () => {
   return (
     <>
-      <div className="grid grid-cols-12 gap-4 bg-gray-100">
-        <div className="md:col-span-2 sm:col-span-0"> {/* side bar */}</div>
-        <div className="md:col-span-8 sm:col-span-12 col-span-12 flex justify-center ">
-          <div className="md:w-3/4 sm:w-full">
+      <Head>
+        <title>Profile</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Navigation />
+      <div className="grid grid-cols-12 gap-4 bg-gray-100 dark:bg-slate-900 pt-2 w-full ">
+        <div className="col-span-2  hidden xl:block h-[89vh] overflow-y-scroll scrollbar	scrollbar-hide hover:scrollbar-default">
+          <LeftSideBar />
+        </div>
+
+        <div className="col-span-12 xl:col-span-10 2xl:col-span-8 sm:col-span-12 h-[91vh] overflow-y-scroll scrollbar	">
+          {/* scrollbar-hide hover:scrollbar-default */}
+          <div className="md:w-3/4 w-full mx-auto">
             <UserProfile />
           </div>
         </div>
-        <div className="md:col-span-2 sm:col-span-0">{/* side bar */}</div>
+
+        <div className="col-span-2 hidden 2xl:block h-[89vh] overflow-y-scroll scrollbar	scrollbar-hide hover:scrollbar-default">
+          <RightSideBar />
+        </div>
       </div>
-       
     </>
   );
 };
