d99badf01722b55d7ed7f6a38bbb20874841037d
shabbir3g <mostafizshabbir@gmail.com>
2022-02-16 21:32:10 +0600

    fixed registration page

--- pages/Register.js
+++ pages/Register.js
@@ -29,6 +29,10 @@ const Register = () => {
     }
   };
 
+  const handleHideConfirm = () => {
+    setShowPassConfirm(true);
+  };
+
   const handleShowConfirm = () => {
     setShowPassConfirm(false);
   };
@@ -41,142 +45,7 @@ const Register = () => {
     setShowPass(false);
   };
 
-  return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-          integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </Head>
-      <div className="flex justify-center m-10 sm:mx-auto md:flex-row flex-col w-5/6">
-        <div className="md:w-3/6">
-          <Image
-            src="http://uitheme.net/sociala/images/login-bg.jpg"
-            height="800"
-            width="700"
-            alt="register image"
-          />
-          <a className="font-bold text-3xl text-blue-600 absolute top-10 left-10">
-            <Image alt="Socio Trend" width="300" height="100" src="/logo.png" />
-          </a>
-        </div>
-        <div className="md:w-2/5 w-11/12 mx-auto pt-10">
-          <h2 className="md:text-4xl text-3xl text-gray-900 font-bold mb-4">
-            Create your account
-          </h2>
-          {/* register-form */}
-          <form onSubmit={handleSubmit(onSubmit)}>
-            {/* name-field */}
-            <input
-              {...register("displayName", { required: true })}
-              className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md"
-              placeholder="Your Name"
-            ></input>
-            <input
-              {...register("email", { required: true })}
-              className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md mt-5"
-              placeholder="Your Email address"
-            ></input>
-
-            {/* password-filed */}
-            <div className="relative">
-              <input
-                {...register("password", { required: true })}
-                type={showPass ? "text" : "password"}
-                className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md mt-5"
-                placeholder="Password"
-              ></input>
-              <i
-                onClick={showPass ? handleShowPass : handleHidePass}
-                className={
-                  showPass
-                    ? "fa-solid fa-eye absolute right-5 top-10 cursor-pointer"
-                    : "fa-solid  fa-eye-slash absolute right-5 top-10 cursor-pointer"
-                }
-              />
-            </div>
-
-            {/* confirm-password */}
-            <div className="relative">
-              <input
-                {...register("confirmPass", { required: true })}
-                type={showPassConfirm ? "text" : "password"}
-                className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md mt-5"
-                placeholder="Confirm Password"
-              ></input>
-              <i
-                onClick={
-                  showPassConfirm ? handleShowConfirm : handleHideConfirm
-                }
-                className={
-                  showPassConfirm
-                    ? "fa-solid fa-eye absolute right-5 top-10 cursor-pointer"
-                    : "fa-solid  fa-eye-slash absolute right-5 top-10 cursor-pointer"
-                }
-              />
-            </div>
-
-            <div className="flex py-3">
-              <input
-                id="terms"
-                type="checkbox"
-                className="w-4 h-4 rounded mt-1"
-              />
-              <label
-                htmlFor="terms"
-                className="text-gray-400 font-semibold pl-2"
-              >
-                Accept Term and Conditions
-              </label>
-            </div>
-            <button
-              type="submit"
-              className="w-full h-14 py-4 font-bold rounded-md bg-gray-900 text-white hover:opacity-75"
-            >
-              Register
-            </button>
-          </form>
-          <p className="text-gray-400 py-3 font-semibold text-center">
-            Already have account{" "}
-            <Link passHref href="/login">
-              <a className="text-orange-500">Login</a>
-            </Link>
-          </p>
-          <div className="text-center">
-            <p className="text-red-500 font-bold">{errorMsg}</p>
-          </div>
-        </div>
-
-        {/* react-toast-for-alert */}
-
-        <div>
-          <ToastContainer />
-        </div>
-      </div>
-    </>
-  );
-};
-
-const handleHideConfirm = () => {
-  setShowPassConfirm(true);
-};
-
-const handleShowConfirm = () => {
-  setShowPassConfirm(false);
-};
-
-const handleHidePass = () => {
-  setShowPass(true);
-};
-
-const handleShowPass = () => {
-  setShowPass(false);
-};
-
+  
 return (
   <>
     <Head>
@@ -202,19 +71,19 @@ return (
       </div>
       <div className="md:w-2/5 w-11/12 mx-auto pt-10">
         <h2 className="md:text-4xl text-3xl text-gray-900 font-bold mb-4">
-          Create <br /> your account
+          Create your account
         </h2>
         {/* register-form */}
         <form onSubmit={handleSubmit(onSubmit)}>
           {/* name-field */}
           <input
             {...register("displayName", { required: true })}
-            className="w-full border border-gray-400 h-14 py-4 pl-12 rounded-md"
+            className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md"
             placeholder="Your Name"
           ></input>
           <input
             {...register("email", { required: true })}
-            className="w-full border border-gray-400 h-14 py-4 pl-12 rounded-md mt-5"
+            className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md mt-5"
             placeholder="Your Email address"
           ></input>
 
@@ -223,15 +92,15 @@ return (
             <input
               {...register("password", { required: true })}
               type={showPass ? "text" : "password"}
-              className="w-full border border-gray-400 h-14 py-4 pl-12 rounded-md mt-5"
+              className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md mt-5"
               placeholder="Password"
             ></input>
             <i
               onClick={showPass ? handleShowPass : handleHidePass}
               className={
                 showPass
-                  ? "fa-solid fa-eye-slash absolute right-5 top-10"
-                  : "fa-solid  fa-eye absolute right-5 top-10"
+                  ? "fa-solid fa-eye absolute right-5 top-10"
+                  : "fa-solid  fa-eye-slash absolute right-5 top-10"
               }
             />
           </div>
@@ -241,15 +110,15 @@ return (
             <input
               {...register("confirmPass", { required: true })}
               type={showPassConfirm ? "text" : "password"}
-              className="w-full border border-gray-400 h-14 py-4 pl-12 rounded-md mt-5"
+              className="w-full border border-gray-400 h-14 py-4 pl-4 rounded-md mt-5"
               placeholder="Confirm Password"
             ></input>
             <i
               onClick={showPassConfirm ? handleShowConfirm : handleHideConfirm}
               className={
                 showPassConfirm
-                  ? "fa-solid fa-eye-slash absolute right-5 top-10"
-                  : "fa-solid  fa-eye absolute right-5 top-10"
+                  ? "fa-solid fa-eye absolute right-5 top-10"
+                  : "fa-solid  fa-eye-slash absolute right-5 top-10"
               }
             />
           </div>
@@ -285,6 +154,7 @@ return (
       </div>
     </div>
   </>
-);
+)
+}
 
 export default Register;
