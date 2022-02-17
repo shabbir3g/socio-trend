47e394565b609b98eb2ff8b8713b49fdf491107d
Prantosh <bepariprantosh360@gmail.com>
2022-02-17 23:32:13 +0600

    fix navigation sidebar

--- components/Share/Navigation.js
+++ components/Share/Navigation.js
@@ -4,12 +4,13 @@ import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useSelector } from "react-redux";
+import NavigationSideBar from "./NavigationSideBar";
 
 const Navigation = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  const user = useSelector((state)=> state.states.user);
+  const user = useSelector((state) => state.states.user);
 
   useEffect(() => setMounted(true), []);
 
@@ -20,13 +21,19 @@ const Navigation = () => {
     const currentTheme = theme === "system" ? systemTheme : theme;
     if (currentTheme === "dark") {
       return (
-        <button className="w-14 h-14 items-center justify-center hidden lg:flex text-yellow-300 text-2xl" onClick={() => setTheme("light")}>
+        <button
+          className="w-14 h-14 items-center justify-center hidden lg:flex text-yellow-300 text-2xl"
+          onClick={() => setTheme("light")}
+        >
           <i className="fa-solid fa-lightbulb"></i>
         </button>
       );
     } else {
       return (
-        <button className="w-14 h-14 items-center justify-center hidden lg:flex text-teal-900 text-2xl" onClick={() => setTheme("dark")}>
+        <button
+          className="w-14 h-14 items-center justify-center hidden lg:flex text-teal-900 text-2xl"
+          onClick={() => setTheme("dark")}
+        >
           <i className="fa-solid fa-moon"></i>
         </button>
       );
@@ -59,26 +66,37 @@ const Navigation = () => {
       </Head>
       <div className="flex items-center bg-neutral-300 py-2 mb-2 dark:bg-gray-800">
         <Link href="/">
-        <a
-          className="font-bold text-4xl pt-1 text-blue-700 pl-10 pr-16"
-        >
-          { (currentTheme === "dark")? <Image alt="Socio Trend" width="200" height="66" src="/dark-logo.png"  /> : <Image alt="Socio Trend" width="200" height="66" src="/logo.png"  /> }
-          
-        
-          
-        </a>
+          <a className="font-bold text-4xl pt-1 text-blue-700 pl-10 pr-16">
+            {currentTheme === "dark" ? (
+              <Image
+                alt="Socio Trend"
+                width="200"
+                height="66"
+                src="/dark-logo.png"
+              />
+            ) : (
+              <Image
+                alt="Socio Trend"
+                width="200"
+                height="66"
+                src="/logo.png"
+              />
+            )}
+          </a>
         </Link>
         <input
           className="w-3/12 h-10  pl-4 rounded-full bg-white hidden lg:flex"
           placeholder="Start typing to search"
         ></input>
         <div className="xl:flex hidden ">
-          <Link href="/"><a
-            href=""
-            className="w-10 h-10 items-center justify-center flex text-gray-400 hover:text-blue-500 text-2xl bg-white hover:bg-blue-100 ml-5 rounded-full"
-          >
-            <i className="fas fa-home text-base"></i>
-          </a></Link>
+          <Link href="/">
+            <a
+              href=""
+              className="w-10 h-10 items-center justify-center flex text-gray-400 hover:text-blue-500 text-2xl bg-white hover:bg-blue-100 ml-5 rounded-full"
+            >
+              <i className="fas fa-home text-base"></i>
+            </a>
+          </Link>
           <a
             href="#"
             className="w-10 h-10 items-center justify-center flex text-gray-400 hover:text-blue-500 text-2xl bg-white hover:bg-blue-100 ml-5 rounded-full"
@@ -92,12 +110,13 @@ const Navigation = () => {
             <i className="fas fa-video text-base"></i>
           </a>
           <Link href="/friends">
-          <a
-            href=""
-            className="w-10 h-10 items-center justify-center flex text-gray-400 hover:text-blue-500 text-2xl bg-white hover:bg-blue-100 ml-5 rounded-full"
-          >
-            <i className="far fa-user text-base"></i>
-          </a></Link>
+            <a
+              href=""
+              className="w-10 h-10 items-center justify-center flex text-gray-400 hover:text-blue-500 text-2xl bg-white hover:bg-blue-100 ml-5 rounded-full"
+            >
+              <i className="far fa-user text-base"></i>
+            </a>
+          </Link>
           <a
             href="#"
             className="w-10 h-10 items-center justify-center flex text-gray-400 hover:text-blue-500 text-2xl bg-white hover:bg-blue-100 ml-5 rounded-full"
@@ -108,12 +127,13 @@ const Navigation = () => {
         <div className="flex ml-auto">
           {renderThemeChanger()}
           <Link href="notification">
-           <a
-            href=""
-            className="w-14 h-14 items-center justify-center hidden lg:flex text-teal-900 dark:text-white text-2xl"
-          >
-            <i className="far fa-bell"></i>
-          </a></Link>
+            <a
+              href=""
+              className="w-14 h-14 items-center justify-center hidden lg:flex text-teal-900 dark:text-white text-2xl"
+            >
+              <i className="far fa-bell"></i>
+            </a>
+          </Link>
           <a
             href="#"
             className="w-14 h-14 items-center justify-center hidden lg:flex text-teal-900 dark:text-white text-2xl"
@@ -128,22 +148,26 @@ const Navigation = () => {
             <i className="far fa-sun"></i>
           </a>
           <Link href="/profile">
-          <a
-            href=""
-            className="w-10 h-10 rounded-full items-center justify-center hidden lg:flex mr-5 mt-1"
-          >
-            <Image
-              alt="user"
-              width="30"
-              height="30"
-              className="rounded-full"
-              src={user?.photoURL || "http://uitheme.net/sociala/images/profile-4.png"}
-            />
-          </a></Link>
+            <a
+              href=""
+              className="w-10 h-10 rounded-full items-center justify-center hidden lg:flex mr-5 mt-1"
+            >
+              <Image
+                alt="user"
+                width="30"
+                height="30"
+                className="rounded-full"
+                src={
+                  user?.photoURL ||
+                  "http://uitheme.net/sociala/images/profile-4.png"
+                }
+              />
+            </a>
+          </Link>
 
           <a
-            href="#"
-            className="w-10 h-10 items-center justify-center lg:hidden flex text-blue-500 text-2xl"
+            href="chat"
+            className="w-14 h-14 items-center justify-center lg:hidden flex text-blue-500 text-2xl"
           >
             <i className="far fa-comment"></i>
           </a>
@@ -167,6 +191,7 @@ const Navigation = () => {
             <i className="fas fa-bars bars"></i>
           </a>
         </div>
+
         <div className="z-50 w-full lg:hidden flex justify-between absolute bottom-0 left-0 right-0 px-5 bg-gradient-to-r from-blue-700 to-cyan-500">
           <a
             href="#"
@@ -192,8 +217,16 @@ const Navigation = () => {
           >
             <i className="fab fa-hive"></i>
           </a>
-          <Link href="/profile"><a href="" className="w-10 h-10 rounded-full items-center justify-center mt-2">
-            <img href="" src="http://uitheme.net/sociala/images/profile-4.png" />
+
+          <Link href="/profile">
+            <a className="w-10 h-10 rounded-full items-center justify-center mt-2">
+              <Image
+                className="rounded-full"
+                src={user?.photoURL ||"http://uitheme.net/sociala/images/profile-4.png"}
+                width="40"
+                height="40"
+                alt="profile"
+              />
             </a>
           </Link>
         </div>
@@ -201,7 +234,7 @@ const Navigation = () => {
           id="slider"
           className="w-6/12 bg-gray-400 absolute -left-2/4 bottom-14  top-20 transition "
         >
-          <h2>opi Barua</h2>
+          <NavigationSideBar />
         </div>
       </div>
     </>
