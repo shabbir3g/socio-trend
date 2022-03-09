470d87b83171fdf342117626d104f3b9ae7fd092
Prantosh <bepariprantosh360@gmail.com>
2022-03-09 13:14:59 +0600

    test

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,27 @@
 const sum = require("./sum");
 
+// .toBe(3)
+
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+// .toEqua
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+// .not.toBe(0)
+
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    console.log(a);
+    for (let b = 1; b < 10; b++) {
+      console.log(b);
+      expect(a + b).not.toBe(0);
+    }
+  }
 });
