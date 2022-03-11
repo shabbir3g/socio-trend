4c7a4c6e835ea7a519e8ebd683d9648fc7a1704f
shabbir3g <mostafizshabbir@gmail.com>
2022-03-11 16:58:13 +0600

    added next config code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://socio-trend.vercel.app/:path*",
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: [
